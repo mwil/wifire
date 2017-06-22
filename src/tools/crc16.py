400a8812671fade70ff14262c697b48b5049c2b2
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2017-06-22 16:49:58 +0900

    Reformatted Python v3

--- src/tools/crc16.py
+++ src/tools/crc16.py
@@ -3,9 +3,10 @@
 #    Author  : Lammert Bies  1999-2008
 #    E-mail  : info@lammertbies.nl
 
+
 def init_crckermit_tab():
     crc_tabkermit = []
-    
+
     for i in range(256):
         crc, c = 0, i
 
@@ -14,33 +15,37 @@ def init_crckermit_tab():
             c >>= 1
 
         crc_tabkermit.append(crc)
-        
+
     return crc_tabkermit
+
 
 CRC_TAB_KERMIT = init_crckermit_tab()
 
+
 def update_crc_kermit(crc, c):
-    return (crc >> 8) ^ CRC_TAB_KERMIT[(crc ^ c) & 0xFF];
+    return (crc >> 8) ^ CRC_TAB_KERMIT[(crc ^ c) & 0xFF]
+
 
 def calc_crc_kermit(data):
     crc = 0
-    
+
     for byte in data:
         crc = update_crc_kermit(crc, byte)
-    
-    # switch byte order    
+
+    # switch byte order
     return ((crc << 8) & 0xFF00) | (crc >> 8)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     packet = [0x41, 0x88, 0x6D, 0x22, 0x00, 0xFF, 0xFF, 0x00,
               0x00, 0x3F, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x04,
               0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    packet = b'\x41\x88\x6D\x22\x00\xFF\xFF\x00\x00\x3F\x00\xFF\xFF\x00\x00\x04\x00\x00\x00\x00\x00\x00'
-    
-    packet = b'\x00\x00\x00\x00\x00\x00\x00\x00\x01\xed\xda\xeb\xfe'
-     
+    packet = b"\x41\x88\x6D\x22\x00\xFF\xFF\x00\x00\x3F\x00"\
+             b"\xFF\xFF\x00\x00\x04\x00\x00\x00\x00\x00\x00"
+
+    packet = b"\x00\x00\x00\x00\x00\x00\x00\x00\x01\xed\xda\xeb\xfe"
+
     crc = calc_crc_kermit(packet)
-    print('CRC16: {:#x}'.format(crc))    
-    
+    print("CRC16: {:#x}".format(crc))
+
     # assert(crc == 0x7ef9)
-    
