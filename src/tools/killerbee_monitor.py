f8d1c7c243d9daa8b7072d72d9375dec568798e3
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2017-06-22 16:58:00 +0900

    Should work with Python 2 again after modifications

--- src/tools/killerbee_monitor.py
+++ src/tools/killerbee_monitor.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
+from __future__ import print_function
+
 import struct
 import sys
 
@@ -35,6 +37,7 @@ logfile = open(filename, "w")
 
 def log(x):
     global logfile
+
     print(x)
     logfile.write(x + "\n")
 
@@ -65,6 +68,7 @@ try:
             except struct.error:
                 log("Undecodeable packet: %s" % (packet))
                 continue
+
             src = data[2]
             dst = data[1]
             seq = data[0]
@@ -74,8 +78,10 @@ try:
                 "%d, FCF:%s, Payload:%s, crc:%s" %
                 (src, repr(packet[4]), dst, repr(packet[2]), seq, len(p[0]),
                  len(packet[7]), repr(packet[0]), repr(packet[7]), crc_state))
+
         elif packet[0][0] == "\x02":  # ack frame
             seq = ord(packet[1])
+
             log("ACK: #%d, crc:%s" % (seq, crc_state))
         else:   # uninteresting packet
             log(packet)
@@ -93,8 +99,10 @@ try:
             parent = data[1]
             etx = data[2]
             # remaining two bytes seem to be crc
+
             log("  CTP Routing: option:%d, parent:%d, etx:%d" %
                 (option, parent, etx))
+
         elif packet[7][:2] == "\x3F\x71":   # ctp data frame
             try:
                 data = struct.unpack("!BBHHBB", packet[7][2:10])
@@ -109,6 +117,7 @@ try:
             seqno = data[4]
             collect_id = data[5]
             payload = packet[7][10:-2]
+
             log("  CTP Data: option:%d, thl:%d, etx:%d, origin:%d, "
                 "seqno:%d, collect_id:%d, data:%s" %
                 (option, thl, etx, origin, seqno, collect_id, repr(payload)))
@@ -126,6 +135,7 @@ try:
             parent = data[3]
             cost = data[4]
             hopcount = data[5]
+
             log("  LQI routing: origin:%d, seqno:%d, originseqno:%d, "
                 "parent:%d, cost:%d, hopcount:%d" %
                 (originaddr, seqno, originseqno, parent, cost, hopcount))
@@ -136,12 +146,14 @@ try:
             except struct.error:
                 print("Error decoding LQI data frame")
                 continue
+
             originaddr = data[0]
             seqno = data[1]
             originseqno = data[2]
             hopcount = data[3]
             collectionid = data[4]
             payload = data[5]
+
             log("  LQI data: origin:%d, seqno:%d, originseqno:%d, "
                 "hopcount:%d, collect_id:%d, data:%d" %
                 (originaddr, seqno, originseqno,
@@ -159,6 +171,7 @@ try:
             duplicates = data[2]
             packetcount = data[3]
             missedcount = data[4]
+
             log("  Measurement packet: framecount:%d, packetcount:%d, "
                 "missedcount:%d, timediff:%d, duplicates:%d" %
                 (framecount, packetcount, missedcount, timediff, duplicates))
--- src/tools/receive_report.py
+++ src/tools/receive_report.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+
 import socket
-from struct import *
+import struct
 
 port = 49155
 
@@ -10,28 +13,35 @@ REP_GENERIC = 8
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", port))
 
-#try:
+# try:
 while True:
     data, addr = sock.recvfrom(1024)
-    report = unpack('!I', data[0:4])
+    report = struct.unpack("!I", data[0:4])
     cmd = report[0]
-    if cmd == REP_MSDU:
-	tmp = unpack('!III', data[4:])
-	msdu_len = tmp[0]
-	msdu_pos = tmp[1]
-	rx_byte = tmp[2]
-	#print 'MSDU: len: %d, pos: %d, payload: %s' % (msdu_len, msdu_pos, repr(data[12:12+msdu_len-8]))
-	print 'MSDU: len: %d, pos: %d, rx_byte: %s' % (msdu_len, msdu_pos, repr(chr(rx_byte % 255)))
-    elif cmd == REP_GENERIC:
-	tmp = unpack('!II', data[4:])
-	key = tmp[0]
-	value = tmp[1]
-	print 'Generic: key: %d, value: %d' % (key, value)
-    elif cmd == REP_TUNE_WIFIRE:
-	tmp = unpack('!IIIIIIIIIIIIIII', data[4:])
-	print 'Header: seqno:%d, srcaddr:%d, srcpan:%d, dstaddr:%d, dstpan:%d, verdict:%d' % (tmp[2], tmp[8], tmp[6], tmp[5], tmp[3], tmp[9])
-    else:
-	print "cmd: %d, len: %d, data: %s" % (cmd, len(data), repr(data))
-#except:
-#    sock.close()
 
+    if cmd == REP_MSDU:
+        tmp = struct.unpack("!III", data[4:])
+        msdu_len = tmp[0]
+        msdu_pos = tmp[1]
+        rx_byte = tmp[2]
+        # print("MSDU: len: %d, pos: %d, payload: %s" %
+        #       (msdu_len, msdu_pos, repr(data[12:12+msdu_len-8])))
+        print("MSDU: len: %d, pos: %d, rx_byte: %s" %
+              (msdu_len, msdu_pos, repr(chr(rx_byte % 255))))
+
+    elif cmd == REP_GENERIC:
+        tmp = struct.unpack("!II", data[4:])
+        key = tmp[0]
+        value = tmp[1]
+        print("Generic: key: %d, value: %d" % (key, value))
+
+    elif cmd == REP_TUNE_WIFIRE:
+        tmp = struct.unpack("!IIIIIIIIIIIIIII", data[4:])
+        print("Header: seqno:%d, srcaddr:%d, srcpan:%d, "
+              "dstaddr:%d, dstpan:%d, verdict:%d" %
+              (tmp[2], tmp[8], tmp[6], tmp[5], tmp[3], tmp[9]))
+
+    else:
+        print("cmd: %d, len: %d, data: %s" % (cmd, len(data), repr(data)))
+# except:
+#    sock.close()
